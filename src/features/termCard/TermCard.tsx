c3bc49286dfc54ad6aaa3b38d766e8551863a773
josh hung <lebjamesron@gmail.com>
2021-12-26 14:12:17 +0800

    implemented styles of header2

--- src/features/termCard/TermCard.tsx
+++ src/features/termCard/TermCard.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TermContent from './TermContent'
 import TermFooter from './TermFooter'
 import TermHeader1 from './TermHeader1'
 import TermHeader2 from './TermHeader2'
 
 const TermCard = () => {
+    const [isShowHeader2, setIsShowHeader2] = useState(true);
     return (
         <div className='w-[400px] h-[300px] border-solid border-[1px]'>
             <TermHeader1/>
-            <TermHeader2/>
+            {
+                isShowHeader2 && 
+                <TermHeader2 onHide={() => setIsShowHeader2(isShow => !isShow)}/>
+            }
             <div>
                 <TermContent/>
             </div>
--- src/features/termCard/TermHeader2.tsx
+++ src/features/termCard/TermHeader2.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
 
-const TermHeader2 = () => {
+export interface TermHeader2Props {
+    onHide?: () => void 
+}
+
+const TermHeader2 = ({onHide}: TermHeader2Props) => {
     return (
-        <div>
-            Header2
-            
-        </div>
+        <header className='bg-gray-500 grid grid-cols-8 text-white text-base font-bold uppercase'>
+            <div className='col-span-7 flex gap-2 items-center justify-center'>
+                <div className='bg-white text-gray-500 p-1 cursor-pointer'>All</div>
+                <div className='cursor-pointer'><a href="" target="_blank">tw</a></div>
+                <div className='cursor-pointer'><a href="" target="_blank">fb</a></div>
+                <div className='cursor-pointer'><a href="" target="_blank">ig</a></div>
+                <div className='cursor-pointer'><a href="" target="_blank">yt</a></div>
+            </div>
+            <div 
+                className='col-span-1 border-solid border-l-[1px] flex justify-center items-center cursor-pointer'
+                onClick={onHide}
+            >
+                <span>
+                    Hide
+                </span>
+            </div>
+        </header>
     )
 }
 
